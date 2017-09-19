fb2e1fbb92e77e2f46f1b4d050f4dc5a72a21e13
myathtut <aa.htut@gmail.com>
2017-09-19 17:48:53 +0630

    tutorilas

---
+++ src/components/TodoList.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <ul>
+            <li>TodoA</li>
+            <li>TodoB</li>
+            <li>TodoC</li>
+            <li>TodoD</li>
+        </ul>
+    </div>
+</template>
+<script type="text/javascript">
+
+    export default {};
+</script>
