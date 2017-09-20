e73bf91052d0ba451602cbe0f5218ca6584654de
myathtut <aa.htut@gmail.com>
2017-09-20 15:33:53 +0630

    todo finish

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,14 +1,33 @@
 <template>
     <div>
-        <ul>
-            <li>TodoA</li>
-            <li>TodoB</li>
-            <li>TodoC</li>
-            <li>TodoD</li>
-        </ul>
+        <p class="tasks">Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+        <p class="tasks">Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+        <todo v-for="todo in todos" :todo.sync="todo" v-bind:todo="todo" v-on:delete-todo="deleteTodo" v-on:complete-todo="completeTodo"></todo>
     </div>
 </template>
 <script type="text/javascript">
+    import Todo from './Todo.vue'
 
-    export default {};
+    export default {
+        props: ['todos'],
+        components: {
+            Todo,
+        },
+        methods: {
+            deleteTodo(todo) {
+                const todoIndex = this.todos.indexOf(todo);
+                console.log('IndexOf', todoIndex);
+                this.todos.splice(todoIndex, 1);
+            },
+            completeTodo(todo) {
+                const todoIndex = this.todos.indexOf(todo);
+                this.todos[todoIndex].done = true;
+            },
+        }
+    };
 </script>
+<style scoped>
+    p.tasks {
+        text-align: center;
+    }
+</style>
